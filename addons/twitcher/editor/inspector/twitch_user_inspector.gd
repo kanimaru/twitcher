f53fe4ea6fd2f2db2e1188d77aeaae00486a9380
kanimaru <kani@kemomi.de>
2025-04-23 23:56:35 +0200

    feat: add possibility to authorize editor directly in inspector

--- addons/twitcher/editor/inspector/twitch_user_inspector.gd
+++ addons/twitcher/editor/inspector/twitch_user_inspector.gd
@@ -3,6 +3,8 @@ extends EditorInspectorPlugin
 
 const TwitchEditorSettings = preload("res://addons/twitcher/editor/twitch_editor_settings.gd")
 const UserProperty = preload("res://addons/twitcher/editor/inspector/user_property.gd")
+const TEST_CREDENTIALS = preload("res://addons/twitcher/editor/setup/test_credentials.tscn")
+const TestCredentials = preload("res://addons/twitcher/editor/setup/test_credentials.gd")
 
 func _can_handle(object: Object) -> bool:
 	return true
@@ -21,6 +23,19 @@ func _parse_property(object: Object, type: Variant.Type, name: String, hint_type
 			info_label.label_settings.font_size = 13
 			info_label.label_settings.font_color = Color.AQUA
 			info_label.autowrap_mode = TextServer.AUTOWRAP_WORD
-			add_custom_control(info_label)
+			info_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+			
+			var authorize_editor: TestCredentials = TEST_CREDENTIALS.instantiate()
+			authorize_editor.text = "Authorize Editor"
+			authorize_editor.oauth_setting = TwitchEditorSettings.editor_oauth_setting
+			authorize_editor.oauth_token = TwitchEditorSettings.editor_oauth_token
+			authorize_editor.disabled = not TwitchEditorSettings.editor_oauth_setting.is_valid()
+			
+			var hbox: HBoxContainer = HBoxContainer.new()
+			hbox.add_child(info_label)
+			hbox.add_child(authorize_editor)
+			hbox.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+			add_custom_control(hbox)
 		
 	return false
+	
