c331c0d27726da9eec7833edbf20fde11af3a49d
kanimaru <kani@kemomi.de>
2025-06-10 18:28:50 +0200

    feat: add test for sprite frame effect

---
+++ tests/sprite_frame_effect.gd
@@ -0,0 +1,10 @@
+﻿extends Control
+
+@export var sprite_frame: SpriteFrames
+
+@onready var rich_text_label: RichTextLabel = $RichTextLabel
+
+func _ready() -> void:
+	var effect: SpriteFrameEffect = SpriteFrameEffect.new()
+	rich_text_label.install_effect(effect)
+	rich_text_label.text = effect.prepare_message("This is a Animated Image: [sprite id='0']%s[/sprite]" % sprite_frame.resource_path, rich_text_label)
