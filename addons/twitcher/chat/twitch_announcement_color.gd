41c45ae091bade08075c4d92bae4c3fda704dd8e
kanimaru <kani@kemomi.de>
2026-04-18 15:09:42 +0200

    feat: add more helper to announcment color

--- addons/twitcher/chat/twitch_announcement_color.gd
+++ addons/twitcher/chat/twitch_announcement_color.gd
@@ -10,5 +10,18 @@ static var PRIMARY: TwitchAnnouncementColor = TwitchAnnouncementColor.new("prima
 
 var value;
 
+static var all_colors: Array[TwitchAnnouncementColor] = [BLUE, GREEN, ORANGE, PURPLE, PRIMARY]
+static var by_name: Dictionary[String, TwitchAnnouncementColor] = {
+	"blue": BLUE, 
+	"green": GREEN, 
+	"orange": ORANGE, 
+	"purple": PURPLE,
+	"primary": PRIMARY
+}
+
+enum Enum {
+	BLUE, GREEN, ORANGE, PURPLE, PRIMARY
+}
+
 func _init(color: String) -> void:
 	value = color;
