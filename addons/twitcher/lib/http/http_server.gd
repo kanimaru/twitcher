99a1569f83b4cc5cdf1341f112337e3c4d9e1e5a
kanimaru <kani@kemomi.de>
2026-05-30 12:03:50 +0200

    chore: white space and type cleanup

--- addons/twitcher/lib/http/http_server.gd
+++ addons/twitcher/lib/http/http_server.gd
@@ -18,17 +18,17 @@ class Server extends TCPServer:
 	signal client_connected(client: Client)
 	signal client_disconnected(client: Client)
 	signal client_error_occured(client: Client, error: Error)
-	
-	
+
+
 	func _init(bind_address: String, port: int) -> void:
 		_bind_address = bind_address
 		_port = port
 		HTTPServer._servers[_port] = weakref(self)
-	
-	
+
+
 	func start_listening() -> void:
 		_listeners += 1
-		if !is_listening(): 
+		if !is_listening():
 			var status: Error = listen(_port, _bind_address)
 			Engine.get_main_loop().process_frame.connect(_process)
 			if status != OK:
@@ -37,23 +37,23 @@ class Server extends TCPServer:
 				HTTPServer.logInfo("{%s:%s} listening" % [ _bind_address, _port ])
 		else:
 			HTTPServer.logDebug("{%s:%s} already listening" % [ _bind_address, _port ])
-	
-	
+
+
 	func stop_listening() -> void:
 		_listeners -= 1
 		HTTPServer.logDebug("{%s:%s} listener node detached %s left" % [ _bind_address, _port, _listeners ])
 		if _listeners <= 0:
 			_stop_server()
 
-	
+
 	func _stop_server() -> void:
 		HTTPServer.logInfo("{%s:%s} stop" % [ _bind_address, _port ])
 		Engine.get_main_loop().process_frame.disconnect(_process)
 		for client in _clients:
 			client.peer.disconnect_from_host()
 		stop()
-		
-		
+
+
 	func _notification(what: int) -> void:
 		if what == NOTIFICATION_PREDELETE:
 			HTTPServer.logInfo("{%s:%s} removed" % [ _bind_address, _port ])
@@ -69,7 +69,7 @@ class Server extends TCPServer:
 		for client in _clients:
 			_process_request(client)
 			_handle_disconnect(client)
-	
+
 	func _process_request(client: Client) -> void:
 		var peer := client.peer
 
@@ -85,8 +85,8 @@ class Server extends TCPServer:
 		var avail := peer.get_available_bytes()
 		if avail > 0:
 			request_received.emit(client)
-	
-	
+
+
 	func _handle_connect() -> void:
 		var peer := take_connection()
 		var client := Client.new()
@@ -94,15 +94,15 @@ class Server extends TCPServer:
 		_clients.append(client)
 		client_connected.emit(client)
 		HTTPServer.logInfo("{%s:%s} client connected" % [ _bind_address, _port ])
-		
-		
+
+
 	func _handle_disconnect(client: Client) -> void:
 		if client.peer.get_status() != StreamPeerTCP.STATUS_CONNECTED:
 			client_disconnected.emit(client)
 			HTTPServer.logInfo("{%s:%s} client disconnected" % [ _bind_address, _port ])
 			_clients.erase(client)
-	
-	
+
+
 class Client extends RefCounted:
 	var peer: StreamPeerTCP
 
@@ -122,7 +122,7 @@ var _listening: bool
 
 
 static func create(port: int, bind_address: String = "*") -> HTTPServer:
-	var server = HTTPServer.new()
+	var server: HTTPServer = HTTPServer.new()
 	server._bind_address = bind_address
 	server._port = port
 	return server
@@ -146,12 +146,12 @@ func _on_request_received(client: Client) -> void:
 
 func _on_client_connected(client: Client) -> void:
 	client_connected.emit(client)
-	
-	
+
+
 func _on_client_disconnected(client: Client) -> void:
 	client_disconnected.emit(client)
-	
-	
+
+
 func _on_client_error_occured(client: Client, error: Error) -> void:
 	client_error_occured.emit(client, error)
 
@@ -164,8 +164,8 @@ func _notification(what: int) -> void:
 func start_listening() -> void:
 	_listening = true
 	_server.start_listening()
-	
-	
+
+
 func stop_listening() -> void:
 	if _listening:
 		_listening = false
@@ -173,7 +173,7 @@ func stop_listening() -> void:
 
 
 func send_response(client: Client, response_code : String, body : PackedByteArray) -> void:
-	var peer = client.peer
+	var peer: StreamPeerTCP = client.peer
 	peer.put_data(("HTTP/1.1 %s\r\n" % response_code).to_utf8_buffer())
 	peer.put_data("Server: Godot Engine (Twitcher)\r\n".to_utf8_buffer())
 	peer.put_data(("Content-Length: %d\r\n"% body.size()).to_utf8_buffer())
@@ -181,7 +181,7 @@ func send_response(client: Client, response_code : String, body : PackedByteArra
 	peer.put_data("Content-Type: text/html charset=UTF-8\r\n".to_utf8_buffer())
 	peer.put_data("\r\n".to_utf8_buffer())
 	peer.put_data(body)
-	
+
 
 
 # === LOGGER ===
