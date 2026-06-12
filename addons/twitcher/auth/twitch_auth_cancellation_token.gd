bc1760568ce240c2fda916e52102e41d63e3e103
Pandacoder <1476550+pandinocoder@users.noreply.github.com>
2026-06-12 13:30:58 +0200

    Apply suggestions from code review
    Co-authored-by: Kani <kani@kemomi.de>

--- addons/twitcher/auth/twitch_auth_cancellation_token.gd
+++ addons/twitcher/auth/twitch_auth_cancellation_token.gd
@@ -6,12 +6,15 @@ signal cancelled()
 
 var _cancelled: bool = false
 
+
 func cancel() -> void:
 	_cancelled = true
 	cancelled.emit()
 
+
 func is_cancelled() -> bool:
 	return _cancelled
+
 
 func wait_until_cancelled() -> void:
 	if _cancelled:
